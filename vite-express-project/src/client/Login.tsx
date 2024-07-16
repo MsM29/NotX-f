422668532ddd8c38b7f0825026c00cb027142e8f
MsM29 <maksim.skripkin31082002@gmail.com>
2024-07-16 18:05:31 +0300

    поправил регистрацию

--- vite-express-project/src/client/Login.tsx
+++ vite-express-project/src/client/Login.tsx
@@ -1,8 +1,46 @@
+import React, { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = JSON.stringify({ email, password });
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      });
+
+      if (!response.ok) {
+        throw new Error("Ошибка входа");
+      }
+      console.log("Вход в систему прошел успешно");
+    } catch (error) {
+      console.error("Ошибка входа:", error);
+    }
+  };
+
   return (
-    <form method="post" action="/login">
-      <input type="email" placeholder="E-MAIL" name="email" required />
-      <input type="password" placeholder="Пароль" name="password" required />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="E-MAIL"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
       <button type="submit">ВОЙТИ</button>
     </form>
   );
--- vite-express-project/src/client/Registration.tsx
+++ vite-express-project/src/client/Registration.tsx
@@ -1,10 +1,66 @@
+import React, { useState } from "react";
+
 function Registration() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [repassword, setRepassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (password === repassword) {
+      const data = JSON.stringify({ name, email, password });
+      try {
+        const response = await fetch("/registration", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: data,
+        });
+
+        if (!response.ok) {
+          throw new Error("Ошибка регистрации");
+        }
+        console.log("Регистрация прошла успешно");
+      } catch (error) {
+        console.error("Ошибка регистрации:", error);
+      }
+    } else {
+      alert("Введенные пароли отличаются");
+    }
+  };
+
   return (
-    <form method="post" action="/registration">
-      <input type="text" name="name" placeholder="Имя пользователя" required />
-      <input type="email" name="email" placeholder="E-MAIL" required />
-      <input type="password" name="password" placeholder="Пароль" required />
-      <input type="password" name="rpassword" placeholder="Повторите пароль" required />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Имя пользователя"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <input
+        type="email"
+        placeholder="E-MAIL"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Повторите пароль"
+        value={repassword}
+        onChange={(e) => setRepassword(e.target.value)}
+        required
+      />
       <button type="submit">ЗАРЕГИСТРИРОВАТЬСЯ</button>
     </form>
   );
